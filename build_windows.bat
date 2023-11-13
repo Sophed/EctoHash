188a14fbd9d0b93125c8e5b241dd9e4bfe547187
june <june0925@proton.me>
2023-11-13 14:10:02 -0500

    remove nonsensical statement

--- build_windows.bat
+++ build_windows.bat
@@ -1,6 +1,6 @@
 @echo off
 
-python3 -m pip --version || pause && goto:EOF
+python3 -m pip --version || pause && exit
 
 pip install pyinstaller packaging customtkinter
 pyinstaller -i icon.ico --onefile app.py
